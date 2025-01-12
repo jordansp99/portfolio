bf8df7e7b4d97eaab355b07ad93fe94e94e079c5
jordansp99 <jordanspeight@hotmail.com>
2025-01-12 01:04:26 +0000

    updated image location

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Button, Avatar } from '@mui/material';
-import jordanImage from '/assets/jordanImage.png'
-
+import '/assets/jordanImage.png'
 import './profile.css'; // Import the CSS file
 
 function Profile() {
@@ -24,7 +23,7 @@ function Profile() {
             </Button>
           </div>
           <div className="promo_column"> 
-            <Avatar alt="photo of Jordan" src={jordanImage} sx={{ width: 300, height: 300 }} />
+            <Avatar alt="photo of Jordan" src="/assets/jordanImage.png" sx={{ width: 300, height: 300 }} />
           </div>
         </div>
         </div>
